8b1de1c02e51073f8238535dd67df24af398bac3
Talha <talhahaider074@gmail.com>
2025-04-21 01:17:42 +0500

    changes done

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@
 // }
 const config = {
     baseUrl:"https://owl-cast-backend.vercel.app/api/v1",
-    baseUrl2:"https://owl-cast-backend.vercel.app/",
+    baseUrl2:"https://owl-cast-backend.vercel.app",
 }
 
 export default config
